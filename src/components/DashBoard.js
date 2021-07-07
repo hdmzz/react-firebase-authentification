b83009892a543517bc586ce707f90718d2578b84
unknown <hdamitzian@gmail.com>
2021-07-08 00:34:12 +0200

    debut dashboard pas encore de logout

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState} from 'react'
+import { Card, Button, Alert } from 'react-bootstrap'
+import { useAuth } from '../contexts/AuthContext'
 
 export default function DashBoard() {
+    const [error, seterror] = useState("")
+    const { currentUser } = useAuth()
+    function handleLogOut() {
+
+    }
+
     return (
-        <div>
-            Dashboard
-        </div>
+        <>
+            <Card>
+                <Card.Body>
+                <h2 className="text-center d-flex mt-10">Profile</h2>
+                {error && <Alert variant="danger">{error}</Alert>}
+                <strong>Email :</strong>{currentUser.email}
+                </Card.Body>
+            </Card>
+            <div className="w-100 text-center mt-4">
+                <Button variant='link' onClick={handleLogOut}>Se Déconnecter</Button>
+            </div>
+        </>
     )
 }
